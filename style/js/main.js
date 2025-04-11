1030f699dfd2c4d920e47c3069075567c29730d5
dsa83171 <dsa83171@gmail.com>
2025-04-11 17:19:30 +0800

    新增自定義資料來源(google sheet)，增加篩選條件

--- style/js/main.js
+++ style/js/main.js
@@ -8,6 +8,11 @@ const app = Vue.createApp({
             isDarkTheme: true,
             showPlayer: false,
             currentEmbedUrl: '',
+            showHelp: false,
+            sheetId:"",
+            sortKey: 'created_at', //預設篩選
+            sortOrder: 'desc', 
+            name: '',
         };
     },
     created() {
@@ -16,19 +21,30 @@ const app = Vue.createApp({
         this.applyTheme();
     },
     mounted(){
+        this.getTitleName();
         this.getClips();
     },
     methods:{
+        getTitleName(){
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            this.name = url.searchParams.get('name');
+            if(this.name == null){
+                this.name = '阿痕 - Twitch剪輯';
+            }
+        },
+        // 切換主題
         toggleTheme() {
             const theme = this.isDarkTheme ? "dark-theme" : "light-theme";
             localStorage.setItem("theme", theme);
             this.applyTheme();
         },
+        // 切換主題
         applyTheme() {
             const theme = this.isDarkTheme ? "dark-theme" : "light-theme";
             document.body.className = theme;
           
-            // 換圓球上的 emoji
+            // 換開關圓球上的 emoji
             this.$nextTick(() => {
                 const sliderParent = document.querySelector(".slider-icon");
                 if (sliderParent) {
@@ -36,11 +52,24 @@ const app = Vue.createApp({
                 }
             });
         },
+        // 取得自定義sheetid
+        getSheetId(){
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            this.sheetId = url.searchParams.get('id');
+        },
+        // 取得Sheet的json
         async getListFromSheet(){
-            var res = await axios.get('https://script.google.com/macros/s/AKfycbxJVw6Lp0h5j0WohEMmWzBpEGtDZYDZcVerr5nqDRyWpzMylEi_uNbA-1Im9Pfddl9r9A/exec?action=getclips');
+            this.getSheetId();
+            var sheetAPI = "https://script.google.com/macros/s/AKfycbxjZUSlhqsKKoLcI3596cQZ8fPYvHnfCJft7Lk2oddMpZWZ902vf5o4saQgHSgnPLtjnQ/exec?action=getclips";
+            if (this.sheetId != null){
+                sheetAPI = sheetAPI+"&sheetid="+this.sheetId;
+            }
+            var res = await axios.get(sheetAPI);
             console.log(res.data);
             this.urls = res.data;
         },
+        // 取得Twitch token
         async getToken(){
             var res = await axios.post('https://id.twitch.tv/oauth2/token', new URLSearchParams({
                 client_id: '2hcw7jubxmk94gkrzhao4wbznzobjv',
@@ -58,9 +87,10 @@ const app = Vue.createApp({
         async getClips(){
             await this.getToken();
             await this.getListFromSheet();
-            // 抽出網址中 clip/ 後面到 ? 前的部分作為 ID
+            //(?:clip\/|clips\.twitch\.tv\/)  匹配 clip/ 或 clips.twitch.tv/ 抓取{id}
             const ids = this.urls.map(item => {
-                const match = item[0].match(/clip\/([^?]+)/);
+                const url = item[0];
+                const match = url.match(/(?:clip\/|clips\.twitch\.tv\/)([^/?]+)/);
                 return match ? match[1] : null;
             }).filter(id => id);
               
@@ -82,23 +112,45 @@ const app = Vue.createApp({
             const secs = Math.floor(seconds % 60);
             return `${mins}:${secs.toString().padStart(2, '0')}`;
         },
+        // 開啟影片撥放器
         openPlayer(clipId) {
             this.currentEmbedUrl = `https://clips.twitch.tv/embed?clip=${clipId}&parent=${window.location.hostname}`;
             this.showPlayer = true;
         },
+        // 關閉影片撥放器
         closePlayer() {
             this.showPlayer = false;
             this.currentEmbedUrl = '';
+        },
+        // 切換升降序
+        toggleSortOrder() {
+            this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+        },
+        // 轉換日期
+        formatLocalTime(utcString) {
+            const date = new Date(utcString);
+            const local = date.toLocaleString(undefined, {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+            }); // 根據使用者語系自動調整
+            return local;
         }
     },
     computed:{
-        totalDuration() {
-            if (!this.clips || !Array.isArray(this.clips.data)) return '0:00';
-
-            const totalSeconds = this.clips.data.reduce((sum, clip) => sum + clip.duration, 0);
-            const mins = Math.floor(totalSeconds / 60);
-            const secs = Math.floor(totalSeconds % 60);
-            return `${mins}:${secs.toString().padStart(2, '0')}`;
+        sortedClips() {
+            if(this.clips !=''){
+                return [...this.clips.data].sort((a, b) => {
+                    const valA = this.sortKey === 'created_at' ? new Date(a[this.sortKey]) : a[this.sortKey];
+                    const valB = this.sortKey === 'created_at' ? new Date(b[this.sortKey]) : b[this.sortKey];
+                
+                    // 字串 or 數字通用比較
+                    if (valA < valB) return this.sortOrder === 'asc' ? -1 : 1;
+                    if (valA > valB) return this.sortOrder === 'asc' ? 1 : -1;
+                    
+                    return 0;
+                });
+            }
         }
     }
 })
